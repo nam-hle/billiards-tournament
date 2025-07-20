6b121bc98d50f6d0703321d979fb990fa17cb491
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-07-20 20:39:21 +0700

    Drop more

--- src/components/forms/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "@/components/forms/login-form";
-export * from "@/components/forms/profile-form";
-export * from "@/components/forms/transaction-form";
--- src/components/tables/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "@/components/tables/bills-table";
-export * from "@/components/tables/transactions-table";
-export * from "@/components/tables/bank-accounts-table";
-export * from "@/components/tables/notifications-table";
