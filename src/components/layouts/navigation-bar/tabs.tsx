cf88acffdd0b1e11883e33bd594c61f36ccca76e
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-07-22 21:51:13 +0700

    Re-enable top navigation animation

--- src/components/layouts/navigation-bar/tabs.tsx
+++ src/components/layouts/navigation-bar/tabs.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import React, { useRef } from "react";
 import { useTheme } from "next-themes";
 import { usePathname } from "next/navigation";
+import React, { useRef, useState, useEffect } from "react";
 
 import { Card, CardContent } from "@/components/shadcn/card";
 
@@ -22,9 +22,8 @@ function useTabs(): Tab[] {
 		const basePath = pathname.split("/").slice(0, 3).join("/");
 
 		return [
-			{ href: pathname, label: "Overview", match: (path) => path === pathname },
+			{ href: basePath, label: "Overview", match: (path) => path === pathname },
 			{ label: "Groups", href: `${basePath}/groups`, match: (path) => path === `${pathname}/groups` },
-			// { label: "Bracket", href: `${basePath}/bracket`, match: (path) => path === `${pathname}/bracket` },
 			{ label: "Schedule", href: `${basePath}/schedule`, match: (path) => path === `${pathname}/schedule` },
 			{ label: "Players", href: `${basePath}/players`, match: (path) => path === `${pathname}/players` },
 			{ label: "Results", href: `${basePath}/results`, match: (path) => path === `${pathname}/results` }
@@ -37,59 +36,60 @@ function useTabs(): Tab[] {
 export const Tabs = () => {
 	const tabs = useTabs();
 	const isHomePage = useHomePage();
+	const pathname = usePathname();
 
-	// const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-	// const [activeIndex, setActiveIndex] = useState(() => tabs.findIndex((tab) => tab.match(pathname)));
-	// const [hoverStyle, setHoverStyle] = useState({});
-	// const [activeStyle, setActiveStyle] = useState({ left: "0px", width: "0px" });
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+	const [activeIndex, setActiveIndex] = useState(() => tabs.findIndex((tab) => tab.match(pathname)));
+	const [hoverStyle, setHoverStyle] = useState({});
+	const [activeStyle, setActiveStyle] = useState({ left: "0px", width: "0px" });
 	const tabRefs = useRef<(HTMLAnchorElement | null)[]>([]);
 
-	// useEffect(() => {
-	// 	if (!tabs.some((tab) => tab.match(pathname))) {
-	// 		setActiveIndex(() => -1);
-	// 		setActiveStyle(() => ({ left: "0px", width: "0px" }));
-	// 	}
-	// }, [pathname, tabs]);
+	useEffect(() => {
+		if (!tabs.some((tab) => tab.match(pathname))) {
+			setActiveIndex(() => -1);
+			setActiveStyle(() => ({ left: "0px", width: "0px" }));
+		}
+	}, [pathname, tabs]);
 
-	// useEffect(() => {
-	// 	if (hoveredIndex !== null) {
-	// 		const hoveredElement = tabRefs.current[hoveredIndex];
-	//
-	// 		if (hoveredElement) {
-	// 			const { offsetLeft, offsetWidth } = hoveredElement;
-	// 			setHoverStyle({
-	// 				left: `${offsetLeft}px`,
-	// 				width: `${offsetWidth}px`
-	// 			});
-	// 		}
-	// 	}
-	// }, [hoveredIndex]);
-	//
-	// useEffect(() => {
-	// 	const activeElement = tabRefs.current[activeIndex];
-	//
-	// 	if (activeElement) {
-	// 		const { offsetLeft, offsetWidth } = activeElement;
-	// 		setActiveStyle({
-	// 			left: `${offsetLeft}px`,
-	// 			width: `${offsetWidth}px`
-	// 		});
-	// 	}
-	// }, [activeIndex]);
-	//
-	// useEffect(() => {
-	// 	requestAnimationFrame(() => {
-	// 		const overviewElement = tabRefs.current[activeIndex];
-	//
-	// 		if (overviewElement) {
-	// 			const { offsetLeft, offsetWidth } = overviewElement;
-	// 			setActiveStyle({
-	// 				left: `${offsetLeft}px`,
-	// 				width: `${offsetWidth}px`
-	// 			});
-	// 		}
-	// 	});
-	// }, [activeIndex]);
+	useEffect(() => {
+		if (hoveredIndex !== null) {
+			const hoveredElement = tabRefs.current[hoveredIndex];
+
+			if (hoveredElement) {
+				const { offsetLeft, offsetWidth } = hoveredElement;
+				setHoverStyle({
+					left: `${offsetLeft}px`,
+					width: `${offsetWidth}px`
+				});
+			}
+		}
+	}, [hoveredIndex]);
+
+	useEffect(() => {
+		const activeElement = tabRefs.current[activeIndex];
+
+		if (activeElement) {
+			const { offsetLeft, offsetWidth } = activeElement;
+			setActiveStyle({
+				left: `${offsetLeft}px`,
+				width: `${offsetWidth}px`
+			});
+		}
+	}, [activeIndex]);
+
+	useEffect(() => {
+		requestAnimationFrame(() => {
+			const overviewElement = tabRefs.current[activeIndex];
+
+			if (overviewElement) {
+				const { offsetLeft, offsetWidth } = overviewElement;
+				setActiveStyle({
+					left: `${offsetLeft}px`,
+					width: `${offsetWidth}px`
+				});
+			}
+		});
+	}, [activeIndex]);
 
 	const { theme } = useTheme();
 	const isDarkMode = theme === "dark";
@@ -102,13 +102,13 @@ export const Tabs = () => {
 		<Card className={`relative flex max-w-[1200px] items-center justify-center border-none shadow-none ${isDarkMode ? "bg-transparent" : ""}`}>
 			<CardContent className="relative flex items-center p-0">
 				{/*/!* Hover Highlight *!/*/}
-				{/*<div*/}
-				{/*	style={{ ...hoverStyle, opacity: hoveredIndex !== null ? 1 : 0 }}*/}
-				{/*	className="absolute flex h-[30px] items-center rounded-[6px] bg-[#0e0f1114] transition-all duration-200 ease-out dark:bg-[#ffffff1a]"*/}
-				{/*/>*/}
+				<div
+					style={{ ...hoverStyle, opacity: hoveredIndex !== null ? 1 : 0 }}
+					className="absolute flex h-[30px] items-center rounded-[6px] bg-[#0e0f1114] transition-all duration-200 ease-out dark:bg-[#ffffff1a]"
+				/>
 
-				{/* Active Indicator */}
-				{/*<div style={activeStyle} className="absolute bottom-[-2px] h-[2px] bg-[#0e0f11] transition-all duration-200 ease-out dark:bg-white" />*/}
+				{/*Active Indicator */}
+				<div style={activeStyle} className="absolute bottom-[-2px] h-[2px] bg-[#0e0f11] transition-all duration-200 ease-out dark:bg-white" />
 
 				{/* Tabs */}
 				<div className="relative flex items-center space-x-[6px]">
@@ -116,11 +116,11 @@ export const Tabs = () => {
 						<Link
 							key={index}
 							href={tab.href}
-							// onMouseLeave={() => setHoveredIndex(null)}
-							// onMouseEnter={() => setHoveredIndex(index)}
-							// onClick={() => {
-							// 	setActiveIndex(index);
-							// }}
+							onMouseLeave={() => setHoveredIndex(null)}
+							onMouseEnter={() => setHoveredIndex(index)}
+							onClick={() => {
+								setActiveIndex(index);
+							}}
 							data-testid={"navigation-item-" + tab.label.toLowerCase()}
 							ref={(el) => {
 								tabRefs.current[index] = el;
