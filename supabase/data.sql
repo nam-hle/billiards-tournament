1ea50241608fbaadbaa95d6779b551b0bcbdf563
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-08-09 12:21:23 +0700

    chore: sync data

--- supabase/data.sql
+++ supabase/data.sql
@@ -192,7 +192,6 @@ INSERT INTO "public"."group_players" ("player_id", "group_id") VALUES
 	('dple', '8808dca7-ab9f-4871-b315-2fbd145f4b2b'),
 	('hqnguyen', '8808dca7-ab9f-4871-b315-2fbd145f4b2b'),
 	('bvphan', 'be8fa313-b24e-4450-841b-d47420aeb726'),
-	('tmdo', 'be8fa313-b24e-4450-841b-d47420aeb726'),
 	('qanguyen', 'be8fa313-b24e-4450-841b-d47420aeb726'),
 	('hnnguyen', 'be8fa313-b24e-4450-841b-d47420aeb726'),
 	('mbnguyen', 'be8fa313-b24e-4450-841b-d47420aeb726'),
@@ -200,7 +199,8 @@ INSERT INTO "public"."group_players" ("player_id", "group_id") VALUES
 	('bvqhuynh', 'ca867865-48a1-458b-9cb9-b43343e0da3b'),
 	('shvu', 'ca867865-48a1-458b-9cb9-b43343e0da3b'),
 	('thnguyen', 'ca867865-48a1-458b-9cb9-b43343e0da3b'),
-	('ltnguyen', 'ca867865-48a1-458b-9cb9-b43343e0da3b');
+	('ltnguyen', 'ca867865-48a1-458b-9cb9-b43343e0da3b'),
+	('benvphan', 'be8fa313-b24e-4450-841b-d47420aeb726');
 
 
 --
@@ -248,50 +248,50 @@ INSERT INTO "public"."matches" ("id", "tournament_id", "type", "player_id_1", "p
 	('37', '2024', 'group', 'tuanttran', 'shvu', 5, 0, NULL, NULL, '2024-08-15 05:00:00+00', 'af349b81-7cbe-482a-99a8-066f8f0e82ed', NULL),
 	('38', '2024', 'group', 'tuanttran', 'dqtrinh', 2, 5, NULL, NULL, '2024-08-22 01:30:00+00', 'af349b81-7cbe-482a-99a8-066f8f0e82ed', NULL),
 	('39', '2024', 'group', 'shvu', 'dqtrinh', 5, 4, NULL, NULL, '2024-08-21 11:15:00+00', 'af349b81-7cbe-482a-99a8-066f8f0e82ed', NULL),
-	('40', '2024', 'quarter-final', 'tmdo', 'dxnguyen', 7, 4, NULL, NULL, '2024-08-24 01:30:00+00', NULL, NULL),
-	('41', '2024', 'quarter-final', 'bvphan', 'ltnguyen', 7, 5, NULL, NULL, '2024-08-24 01:30:00+00', NULL, NULL),
-	('42', '2024', 'quarter-final', 'hqtran', 'tttran', 2, 7, NULL, NULL, '2024-08-24 01:30:00+00', NULL, NULL),
-	('43', '2024', 'quarter-final', 'hqnguyen', 'nhle', 2, 7, NULL, NULL, '2024-08-24 01:30:00+00', NULL, NULL),
-	('44', '2024', 'semi-final', 'tmdo', 'bvphan', 6, 9, NULL, NULL, '2024-08-24 03:00:00+00', NULL, NULL),
-	('45', '2024', 'semi-final', 'tttran', 'nhle', 6, 9, NULL, NULL, '2024-08-24 03:00:00+00', NULL, NULL),
-	('46', '2024', 'final', 'nhle', 'bvphan', 11, 5, NULL, NULL, '2024-08-25 08:00:00+00', NULL, NULL),
 	('47', '2025', 'group', 'nhle', 'hqtran', 5, 0, NULL, NULL, '2025-08-04 05:00:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
-	('48', '2025', 'group', 'atvo', 'nhle', 0, 5, NULL, NULL, '2025-08-06 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('49', '2025', 'group', 'nhle', 'dple', 5, 0, NULL, NULL, '2025-08-03 01:45:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('50', '2025', 'group', 'hqnguyen', 'nhle', 5, 0, NULL, NULL, '2025-08-05 11:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('51', '2025', 'group', 'hqtran', 'atvo', 1, 5, NULL, NULL, '2025-08-05 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
-	('52', '2025', 'group', 'dple', 'hqtran', 0, 5, NULL, NULL, '2025-08-07 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('53', '2025', 'group', 'hqtran', 'hqnguyen', NULL, NULL, NULL, NULL, NULL, '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
-	('54', '2025', 'group', 'atvo', 'dple', NULL, NULL, NULL, NULL, '2025-08-09 02:00:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('55', '2025', 'group', 'hqnguyen', 'atvo', 5, 0, NULL, NULL, '2025-08-04 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
 	('56', '2025', 'group', 'dple', 'hqnguyen', 2, 5, NULL, NULL, '2025-08-03 02:30:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
-	('57', '2025', 'group', 'bvphan', 'tmdo', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('58', '2025', 'group', 'qanguyen', 'bvphan', 5, 3, NULL, NULL, '2025-08-03 01:45:00+00', 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('59', '2025', 'group', 'bvphan', 'hnnguyen', 5, 1, NULL, NULL, '2025-08-05 05:15:00+00', 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('60', '2025', 'group', 'mbnguyen', 'bvphan', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
-	('61', '2025', 'group', 'tmdo', 'qanguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
-	('62', '2025', 'group', 'hnnguyen', 'tmdo', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
-	('63', '2025', 'group', 'tmdo', 'mbnguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
-	('64', '2025', 'group', 'qanguyen', 'hnnguyen', 5, 0, NULL, NULL, '2025-08-06 05:15:00+00', 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('65', '2025', 'group', 'mbnguyen', 'qanguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('66', '2025', 'group', 'hnnguyen', 'mbnguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('67', '2025', 'group', 'tttran', 'bvqhuynh', 5, 0, NULL, NULL, '2025-08-05 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
 	('68', '2025', 'group', 'shvu', 'tttran', 1, 5, NULL, NULL, '2025-08-04 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
 	('69', '2025', 'group', 'tttran', 'thnguyen', 5, 1, NULL, NULL, '2025-08-03 01:45:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('70', '2025', 'group', 'ltnguyen', 'tttran', NULL, NULL, NULL, NULL, '2025-08-08 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('71', '2025', 'group', 'bvqhuynh', 'shvu', NULL, NULL, NULL, NULL, '2025-08-09 02:00:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('72', '2025', 'group', 'thnguyen', 'bvqhuynh', 5, 1, NULL, NULL, '2025-08-07 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('73', '2025', 'group', 'bvqhuynh', 'ltnguyen', NULL, NULL, NULL, NULL, '2025-08-09 03:30:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('74', '2025', 'group', 'shvu', 'thnguyen', NULL, NULL, NULL, NULL, '2025-08-08 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('75', '2025', 'group', 'ltnguyen', 'shvu', 5, 1, NULL, NULL, '2025-08-07 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('61', '2025', 'group', 'benvphan', 'qanguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
+	('52', '2025', 'group', 'dple', 'hqtran', 0, 5, NULL, NULL, '2025-08-07 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
+	('48', '2025', 'group', 'atvo', 'nhle', 0, 5, NULL, NULL, '2025-08-06 05:15:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
+	('64', '2025', 'group', 'qanguyen', 'hnnguyen', 5, 0, NULL, NULL, '2025-08-06 05:15:00+00', 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
 	('76', '2025', 'group', 'thnguyen', 'ltnguyen', 5, 4, NULL, NULL, '2025-08-06 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
-	('77', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#1 of the quarter-finalists', '#8 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, NULL),
-	('78', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#3 of the quarter-finalists', '#6 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, NULL),
-	('79', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#2 of the quarter-finalists', '#7 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, NULL),
-	('80', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#4 of the quarter-finalists', '#5 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, NULL),
-	('81', '2025', 'semi-final', NULL, NULL, NULL, NULL, 'The winner of match #77', 'The winner of match #78', '2025-08-17 03:00:00+00', NULL, NULL),
-	('82', '2025', 'semi-final', NULL, NULL, NULL, NULL, 'The winner of match #79', 'The winner of match #80', '2025-08-17 03:00:00+00', NULL, NULL),
-	('83', '2025', 'final', NULL, NULL, NULL, NULL, 'The winner of match #81', 'The winner of match #82', '2025-08-24 01:30:00+00', NULL, NULL);
+	('75', '2025', 'group', 'ltnguyen', 'shvu', 5, 1, NULL, NULL, '2025-08-07 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('72', '2025', 'group', 'thnguyen', 'bvqhuynh', 5, 1, NULL, NULL, '2025-08-07 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('63', '2025', 'group', 'benvphan', 'mbnguyen', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
+	('62', '2025', 'group', 'hnnguyen', 'benvphan', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
+	('57', '2025', 'group', 'bvphan', 'benvphan', NULL, NULL, NULL, NULL, NULL, 'be8fa313-b24e-4450-841b-d47420aeb726', NULL),
+	('77', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#1 of the quarter-finalists', '#8 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, 0),
+	('70', '2025', 'group', 'ltnguyen', 'tttran', 5, 3, NULL, NULL, '2025-08-08 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('78', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#3 of the quarter-finalists', '#6 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, 2),
+	('79', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#2 of the quarter-finalists', '#7 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, 1),
+	('80', '2025', 'quarter-final', NULL, NULL, NULL, NULL, '#4 of the quarter-finalists', '#5 of the quarter-finalists', '2025-08-17 01:30:00+00', NULL, 3),
+	('81', '2025', 'semi-final', NULL, NULL, NULL, NULL, 'The winner of match #77', 'The winner of match #78', '2025-08-17 03:00:00+00', NULL, 0),
+	('82', '2025', 'semi-final', NULL, NULL, NULL, NULL, 'The winner of match #79', 'The winner of match #80', '2025-08-17 03:00:00+00', NULL, 1),
+	('83', '2025', 'final', NULL, NULL, NULL, NULL, 'The winner of match #81', 'The winner of match #82', '2025-08-24 01:30:00+00', NULL, 0),
+	('40', '2024', 'quarter-final', 'tmdo', 'dxnguyen', 7, 4, NULL, NULL, '2024-08-24 01:30:00+00', NULL, 0),
+	('41', '2024', 'quarter-final', 'bvphan', 'ltnguyen', 7, 5, NULL, NULL, '2024-08-24 01:30:00+00', NULL, 1),
+	('42', '2024', 'quarter-final', 'hqtran', 'tttran', 2, 7, NULL, NULL, '2024-08-24 01:30:00+00', NULL, 2),
+	('43', '2024', 'quarter-final', 'hqnguyen', 'nhle', 2, 7, NULL, NULL, '2024-08-24 01:30:00+00', NULL, 3),
+	('44', '2024', 'semi-final', 'tmdo', 'bvphan', 6, 9, NULL, NULL, '2024-08-24 03:00:00+00', NULL, 0),
+	('45', '2024', 'semi-final', 'tttran', 'nhle', 6, 9, NULL, NULL, '2024-08-24 03:00:00+00', NULL, 1),
+	('46', '2024', 'final', 'nhle', 'bvphan', 11, 5, NULL, NULL, '2024-08-25 08:00:00+00', NULL, 0),
+	('74', '2025', 'group', 'shvu', 'thnguyen', 4, 5, NULL, NULL, '2025-08-08 05:15:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('54', '2025', 'group', 'atvo', 'dple', 2, 5, NULL, NULL, '2025-08-09 02:00:00+00', '8808dca7-ab9f-4871-b315-2fbd145f4b2b', NULL),
+	('71', '2025', 'group', 'bvqhuynh', 'shvu', 2, 5, NULL, NULL, '2025-08-09 02:00:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL),
+	('73', '2025', 'group', 'bvqhuynh', 'ltnguyen', 0, 5, NULL, NULL, '2025-08-09 03:30:00+00', 'ca867865-48a1-458b-9cb9-b43343e0da3b', NULL);
 
 
 --
@@ -301,31 +301,7 @@ INSERT INTO "public"."matches" ("id", "tournament_id", "type", "player_id_1", "p
 
 
 --
--- Data for Name: buckets_analytics; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: iceberg_namespaces; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: iceberg_tables; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
 -- Data for Name: objects; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: prefixes; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
 --
 
 
@@ -343,23 +319,10 @@ INSERT INTO "public"."matches" ("id", "tournament_id", "type", "player_id_1", "p
 
 
 --
--- Data for Name: hooks; Type: TABLE DATA; Schema: supabase_functions; Owner: supabase_functions_admin
---
-
-
-
---
 -- Name: refresh_tokens_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: supabase_auth_admin
 --
 
 SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
-
-
---
--- Name: hooks_id_seq; Type: SEQUENCE SET; Schema: supabase_functions; Owner: supabase_functions_admin
---
-
-SELECT pg_catalog.setval('"supabase_functions"."hooks_id_seq"', 1, false);
 
 
 --
